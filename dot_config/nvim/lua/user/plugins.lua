8066e0604b4591a8d09ccb715097e877b124c3b6
Oscar Ahlén <github@ahlen.dev>
2023-05-22 19:19:29 +0200

    Add gitsigns

--- dot_config/nvim/lua/user/plugins.lua
+++ dot_config/nvim/lua/user/plugins.lua
@@ -201,7 +201,18 @@ require("lazy").setup({
     },
 
     {
-        dir = "/home/oahlen/git/verdandi.nvim/"
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require('gitsigns').setup {
+                signs = {
+                    add          = { text = '+' },
+                    change       = { text = '~' },
+                    delete       = { text = '-' },
+                    topdelete    = { text = '‾' },
+                    changedelete = { text = '~' },
+                    untracked    = { text = '┆' },
+                },
+            }
+        end
     }
-
 })
