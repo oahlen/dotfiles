c014f445808204799518e164b9b4ecf08785d2d0
Oscar Ahlén <github@ahlen.dev>
2023-06-07 20:27:06 +0200

    Update fzf options

--- dot_config/fish/conf.d/variables.fish
+++ dot_config/fish/conf.d/variables.fish
@@ -12,10 +12,12 @@ set -gx CARGO_HOME $XDG_DATA_HOME/cargo
 set -gx DOCKER_CONFIG $XDG_CONFIG_HOME/docker
 set -gx FZF_DEFAULT_COMMAND 'fd --type f --hidden --follow'
 set -gx FZF_DEFAULT_OPTS $FZF_DEFAULT_OPTS '
+    --layout=reverse --scrollbar '▌'
     --color=fg:#c6c8d1,bg:#161821,hl:#e2a478
     --color=fg+:#d2d4de,bg+:#242940,hl+:#e9b189
-    --color=header:#84a0c6,info:#444b71,border:#444b71,separator:#444b71
+    --color=header:#84a0c6,info:#444b71,border:#6b7089,separator:#6b7089
     --color=prompt:#b4be82,pointer:#e2a478,marker:#89b8c2,spinner:#a093c7
+    --color=gutter:#1e2132,scrollbar:#84a0c6
 '
 set -gx JQ_COLORS "1;35:0;35:0;35:0;35:0;36:0;37:0;37"
 set -gx LESSHISTFILE $XDG_DATA_HOME/less/history
@@ -27,6 +29,8 @@ set -gx PGPASSFILE $XDG_CONFIG_HOME/pg/pgpass
 set -gx PSQL_HISTORY $XDG_DATA_HOME/psql_history
 set -gx PYENV_ROOT $XDG_DATA_HOME/pyenv 
 set -gx PYTHONSTARTUP $XDG_CONFIG_HOME/python/pythonrc
-set -gx _ZO_FZF_OPTS $FZF_DEFAULT_OPTS ''
+set -gx _ZO_FZF_OPTS $FZF_DEFAULT_OPTS '--height=40%'
 
 set -gx SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
+
+set -gx ZK_NOTEBOOK_DIR $HOME/notes/
