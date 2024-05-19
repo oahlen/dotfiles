0a26c438830261bd531a3fe3e8b198b95ca1670e
Oscar Ahlén <github@ahlen.dev>
2024-05-19 14:37:57 +0200

    Add system config

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Getting started:
 ```bash
 chezmoi init https://github.com/oahlen/dotfiles.git
 ```
+
+## System install
+
+### fstab
+
+Add the following options: `noatime,nodiratime,discard`
---
+++ system/config.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+cp etc/environment /etc/environment
+cp etc/locale.conf /etc/locale.conf
+cp etc/sysctl.d/99-custom.conf /etc/sysctl.d/99-custom.conf
+cp etc/systemd/journald.conf /etc/systemd/journald.conf
