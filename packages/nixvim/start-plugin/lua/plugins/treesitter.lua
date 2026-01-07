ace5849ef85e7a6786b3d0b9671b7a1dd55f43a2
Oscar Ahlén <github@ahlen.dev>
2026-01-07 09:44:27 +0100

    Migrate treesitter config

--- packages/nixvim/start-plugin/lua/plugins/treesitter.lua
+++ packages/nixvim/start-plugin/lua/plugins/treesitter.lua
@@ -1,13 +1,13 @@
-require("nvim-treesitter.configs").setup({
-    ensure_installed = {},
-    sync_install = false,
-    ignore_install = {},
-    highlight = {
-        enable = true,
-        disable = {},
-        additional_vim_regex_highlighting = false,
-    },
-    indent = {
-        enable = true,
-    },
+vim.api.nvim_create_autocmd("FileType", {
+    group = vim.api.nvim_create_augroup("treesitter.setup", {}),
+    callback = function(args)
+        local filetype = args.match
+
+        local language = vim.treesitter.language.get_lang(filetype) or filetype
+        if not vim.treesitter.language.add(language) then
+            return
+        end
+
+        vim.treesitter.start(args.buf, language)
+    end,
 })
