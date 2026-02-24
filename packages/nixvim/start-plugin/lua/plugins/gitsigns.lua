fd6b9e8ec70f528573aea623e5b03b918eedad9f
Oscar Ahlén <github@ahlen.dev>
2026-02-24 13:21:08 +0100

    Update gitsigns config

--- packages/nixvim/start-plugin/lua/plugins/gitsigns.lua
+++ packages/nixvim/start-plugin/lua/plugins/gitsigns.lua
@@ -1,17 +1,15 @@
 require("gitsigns").setup({
-    signs = {
-        add = { text = "+" },
-        change = { text = "~" },
-        delete = { text = "_" },
-        topdelete = { text = "‾" },
-        changedelete = { text = "~" },
-        untracked = { text = "┆" },
-    },
     on_attach = function(bufnr)
-        local gs = package.loaded.gitsigns
-        vim.keymap.set("n", "<space>hb", function()
-            gs.blame_line({ full = true })
+        local gitsigns = require("gitsigns")
+
+        vim.keymap.set("n", "<leader>hb", function()
+            gitsigns.blame_line({ full = true })
         end, { desc = "Blame Line", buffer = bufnr })
-        vim.keymap.set("n", "<space>ht", gs.toggle_current_line_blame, { desc = "Toggle Line Blame", buffer = bufnr })
+
+        vim.keymap.set("n", "<leader>hd", gitsigns.diffthis, { desc = "Diff this", buffer = bufnr })
+
+        vim.keymap.set("n", "<leader>hD", function()
+            gitsigns.diffthis("~")
+        end, { desc = "Diff this (~)", buffer = bufnr })
     end,
 })
