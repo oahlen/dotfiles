d9217740966b37035e007c1a9771913514ea784e
Oscar Ahlén <github@ahlen.dev>
2026-03-11 13:16:43 +0100

    Delete commented autocommand

--- homes/common/.config/nvim/lua/config/commands.lua
+++ homes/common/.config/nvim/lua/config/commands.lua
@@ -57,29 +57,3 @@ vim.api.nvim_create_user_command("ToggleBackground", function()
         vim.o.background = "dark"
     end
 end, {})
-
--- vim.api.nvim_create_autocmd("BufWritePost", {
---     pattern = "*.cs",
---     callback = function()
---         for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
---             if vim.api.nvim_buf_is_loaded(bufnr) and vim.bo[bufnr].filetype == "cs" then
---                 local clients = vim.lsp.get_clients({ bufnr = bufnr, name = "csharp_ls" })
---                 for _, client in ipairs(clients) do
---                     local params = vim.lsp.util.make_text_document_params(bufnr)
---                     ---@diagnostic disable-next-line: param-type-mismatch
---                     client.notify("textDocument/didChange", {
---                         textDocument = {
---                             uri = params.uri,
---                             version = vim.lsp.util.buf_versions[bufnr] or 0,
---                         },
---                         contentChanges = {
---                             {
---                                 text = table.concat(vim.api.nvim_buf_get_lines(bufnr, 0, -1, false), "\n") .. "\n",
---                             },
---                         },
---                     })
---                 end
---             end
---         end
---     end,
--- })
