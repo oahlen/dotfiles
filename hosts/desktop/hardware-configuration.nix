08b5f4899891ba4349a510cfe0de8c8021ed915a
Oscar Ahlén <github@ahlen.dev>
2026-05-09 13:28:50 +0200

    Update hardware configuration

--- hosts/desktop/hardware-configuration.nix
+++ hosts/desktop/hardware-configuration.nix
@@ -29,6 +29,11 @@
     fsType = "ext4";
   };
 
+  fileSystems."/mnt/backup" = {
+    device = "/dev/disk/by-uuid/96a3132c-4391-482f-b729-82d55a495496";
+    fsType = "ext4";
+  };
+
   fileSystems."/boot" = {
     device = "/dev/disk/by-uuid/40F6-63AD";
     fsType = "vfat";
@@ -38,21 +43,7 @@
     ];
   };
 
-  fileSystems."/mnt/backup" = {
-    device = "/dev/disk/by-uuid/96a3132c-4391-482f-b729-82d55a495496";
-    fsType = "ext4";
-  };
-
   swapDevices = [ ];
-
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
-  # networking.interfaces.tailscale0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlan0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
