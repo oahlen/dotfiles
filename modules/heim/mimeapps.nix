fdea1e76c8be1256d989a49e17aef6c22d5adc13
Oscar Ahlén <github@ahlen.dev>
2026-05-12 16:39:27 +0200

    Refactor mimeapps

--- modules/heim/mimeapps.nix
+++ modules/heim/mimeapps.nix
@@ -1,42 +1,36 @@
-{ ... }:
+{
+  lib,
+  ...
+}:
+let
+  added = { };
+
+  default = {
+    "application/pdf" = "firefox.desktop";
+    "application/x-extension-htm" = "firefox.desktop";
+    "application/x-extension-html" = "firefox.desktop";
+    "application/x-extension-shtml" = "firefox.desktop";
+    "application/x-extension-xht" = "firefox.desktop";
+    "application/x-extension-xhtml" = "firefox.desktop";
+    "application/xhtml+xml" = "firefox.desktop";
+    "image/jpeg" = "org.gnome.Loupe.desktop";
+    "image/png" = "org.gnome.Loupe.desktop";
+    "inode/directory" = "org.gnome.Nautilus.desktop";
+    "text/html" = "firefox.desktop";
+    "text/plain" = "org.gnome.TextEditor.desktop";
+    "x-scheme-handler/chrome" = "firefox.desktop";
+    "x-scheme-handler/http" = "firefox.desktop";
+    "x-scheme-handler/https" = "firefox.desktop";
+  };
+
+  removed = { };
+in
 {
   xdg.config.files = {
-    "mimeapps.list".text = ''
-      [Added Associations]
-      application/pdf=firefox.desktop
-      application/x-extension-htm=firefox.desktop
-      application/x-extension-html=firefox.desktop
-      application/x-extension-shtml=firefox.desktop
-      application/x-extension-xht=firefox.desktop
-      application/x-extension-xhtml=firefox.desktop
-      application/xhtml+xml=firefox.desktop
-      image/jpeg=org.gnome.Loupe.desktop
-      image/png=org.gnome.Loupe.desktop
-      inode/directory=org.gnome.Nautilus.desktop
-      text/html=firefox.desktop
-      text/plain=org.gnome.TextEditor.desktop
-      x-scheme-handler/chrome=firefox.desktop
-      x-scheme-handler/http=firefox.desktop
-      x-scheme-handler/https=firefox.desktop
-
-      [Default Applications]
-      application/pdf=firefox.desktop
-      application/x-extension-htm=firefox.desktop
-      application/x-extension-html=firefox.desktop
-      application/x-extension-shtml=firefox.desktop
-      application/x-extension-xht=firefox.desktop
-      application/x-extension-xhtml=firefox.desktop
-      application/xhtml+xml=firefox.desktop
-      image/jpeg=org.gnome.Loupe.desktop
-      image/png=org.gnome.Loupe.desktop
-      inode/directory=org.gnome.Nautilus.desktop
-      text/html=firefox.desktop
-      text/plain=org.gnome.TextEditor.desktop
-      x-scheme-handler/chrome=firefox.desktop
-      x-scheme-handler/http=firefox.desktop
-      x-scheme-handler/https=firefox.desktop
-
-      [Removed Associations]
-    '';
+    "mimeapps.list".text = lib.generators.toINI { } {
+      "Added Associations" = added;
+      "Default Applications" = default;
+      "Removed Associations" = removed;
+    };
   };
 }
