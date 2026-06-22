d509459bc7db79665b58ffa5757664be56104722
Oscar Ahlén <github@ahlen.dev>
2026-06-22 19:25:42 +0200

    Update base16 theme

--- packages/base16-theme/default.nix
+++ packages/base16-theme/default.nix
@@ -4,6 +4,6 @@
 fetchFromGitHub {
   owner = "oahlen";
   repo = "base16.tmTheme";
-  rev = "b1822cc85b92a7ff578e4df1e4faa3f3a2e89b4f";
-  sha256 = "sha256-GGjR8cQmvP9sNrvp7ZuS+kyQ6ZQSATsUAKOqlzgxFtA=";
+  rev = "83fe109c88668d1ae78851fa66c1c06957d82c52";
+  sha256 = "sha256-1L9m48twPsBAVCmWnxggFjiLgDDHJz92lNfW+UBtTxs=";
 }
