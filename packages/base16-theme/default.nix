1fe4a8ea0c3cd81e71c6e46ad8a0439cef74c063
Oscar Ahlén <github@ahlen.dev>
2026-06-22 17:33:11 +0200

    Consume base16 theme externally

--- packages/base16-theme/default.nix
+++ packages/base16-theme/default.nix
@@ -1,4 +1,9 @@
-{ runCommand }:
-runCommand "base16.tmTheme" { } ''
-  cp ${./base16.tmTheme} $out
-''
+{
+  fetchFromGitHub,
+}:
+fetchFromGitHub {
+  owner = "oahlen";
+  repo = "base16.tmTheme";
+  rev = "b1822cc85b92a7ff578e4df1e4faa3f3a2e89b4f";
+  sha256 = "sha256-GGjR8cQmvP9sNrvp7ZuS+kyQ6ZQSATsUAKOqlzgxFtA=";
+}
