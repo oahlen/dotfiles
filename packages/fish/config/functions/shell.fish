977bd9ed5c6b79e24427c5d45a85c59ef5ead55e
Oscar Ahlén <github@ahlen.dev>
2026-09-26 14:44:41 +0200

    Break out general funcs as scripts

--- packages/fish/config/functions/shell.fish
+++
@@ -1,8 +0,0 @@
-function shell --description "Spawns a nix shell with the given packages"
-    if test (count $argv) -eq 0
-        echo "Usage: shell <package> [packages...]" >&2
-        return 1
-    end
-
-    nix-shell -p $argv --command "export NIX_SHELL='Ephemeral Shell' && fish"
-end
--- packages/fish/config/functions/spawn.fish
+++
@@ -1,9 +0,0 @@
-function spawn --description "Executes a completely detached process"
-    if test (count $argv) -eq 0
-        echo "Usage: spawn <command> [args...]" >&2
-        return 1
-    end
-
-    nohup $argv &>/dev/null &
-    disown
-end
